068460b119c94120a1e2af0e3d90d60bb7d08271
link8430 <link8430@gmail.com>
2025-07-11 12:43:00 +0900

    09 solve

--- CodingTest/src/step3/Main09.java
+++ CodingTest/src/step3/Main09.java
@@ -1,5 +1,21 @@
 package step3;
 
-public class Main09 {
+import java.util.Scanner;
 
+public class Main09 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int count = 0;
+		
+		for(int i =1; i<=N; i++) {
+			count++;
+			for(int j=1; j<=count; j++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+		sc.close();
+	}
 }
