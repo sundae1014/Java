fe5cef0fd9f7f17f2bb2040abc502c03be765788
link8430 <link8430@gmail.com>
2025-07-11 17:47:30 +0900

    array

--- CodingTest/src/step4/Main06.java
+++ CodingTest/src/step4/Main06.java
@@ -1,5 +1,19 @@
 package step4;
 
-public class Main06 {
+import java.util.Scanner;
 
+public class Main06 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[30];
+		
+		for(int i=0; i<30; i++) {
+			arr[i] = sc.nextInt();
+			
+			
+		}
+		
+		sc.close();
+	}
 }
