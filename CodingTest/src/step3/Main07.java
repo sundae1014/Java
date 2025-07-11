d9ab627ce6f22352d2529763860d6409d9dfda62
link8430 <link8430@gmail.com>
2025-07-11 12:25:30 +0900

    solve 07

--- CodingTest/src/step3/Main07.java
+++ CodingTest/src/step3/Main07.java
@@ -1,5 +1,19 @@
 package step3;
 
-public class Main07 {
+import java.util.Scanner;
 
+public class Main07 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int i = 1; i<=T; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			System.out.println("Case #" + i + ": " + (A+B));
+		}
+		sc.close();
+	}
 }
