968013c91179bd2d2b61689424c9166488a6976c
link8430 <link8430@gmail.com>
2025-07-14 14:50:48 +0900

    7월14일 5교시

--- CodingTest/src/step4/Main08.java
+++ CodingTest/src/step4/Main08.java
@@ -6,22 +6,24 @@ public class Main08 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 				
-		int[] a = new int[0];
-		int total = 0;
+		int[] a = new int[10];
+		int total = 10;
 		
 		for(int i =0; i<10; i++) {
-			int[] arr = new int[0];
+			int[] arr = new int[10];
 			arr[i] = sc.nextInt();
 			
 			for(int j=i; j<10; j++) {
 				arr[i] = arr[i]%42;
-				arr[i] = a[i];
-				total += a[i];
+				// arr[i] = a[i];
 			}
 			
 			if(arr[i] == a[i]) {
 				total--;
-			}			
+			}
+			//else {
+			//	total++;
+			//}
 		}
 		System.out.println(total);
 		sc.close();
