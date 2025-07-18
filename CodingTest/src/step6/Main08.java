4b46ba930bc06873f0b7045be36810363f044b1e
link8430 <link8430@gmail.com>
2025-07-18 14:01:42 +0900

    7/18 ;;;

--- CodingTest/src/step6/Main08.java
+++ CodingTest/src/step6/Main08.java
@@ -1,7 +1,35 @@
 package step6;
 
+import java.util.Scanner;
+
 public class Main08 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		for(int i=0; i<20; i++) {
+			String sub = sc.next();
+			double score = sc.nextDouble();
+			String grade = sc.next();
+		
+			double[] a = new  double [i];
+		}	
+		sc.close();
 	}
 }
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
