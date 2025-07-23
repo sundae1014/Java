521f291c1c35f00359747370ea7326c773a66ca0
link8430 <link8430@gmail.com>
2025-07-23 09:50:46 +0900

    7/23 1st

---
+++ ch09/src/sub2/BufferStreamTest.java
@@ -0,0 +1,57 @@
+package sub2;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/*
+ * 	날짜 : 2025/07/23
+ * 	이름 : 정순권
+ * 	내용 : 버퍼 스트림 실습
+ */
+public class BufferStreamTest {
+	public static void main(String[] args) {
+
+		String source = "C:\\Users\\GGG\\Desktop\\Person.tif";
+		String target = "C:\\Users\\GGG\\Desktop\\Person2.tif";
+		
+		try {
+			// 바이트 스트림 생성(파일 연결)
+			FileInputStream fis = new FileInputStream(source);
+			FileOutputStream fos = new FileOutputStream(target);
+			
+			
+			
+			// 무한반복으로 스트림 작업 수행
+			while(true) {
+				
+				// 파일 읽기
+				int data = fis.read();
+
+				if(data == -1) {
+					// 파일 내용이 없으면
+					break;
+				}
+
+				 // 파일 쓰기
+				 fos.write(data);				
+			}
+			
+			// 스트림 작업 종료
+			fis.close();
+			fos.close();
+	
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("프로그램 종료...");
+	}
+		
+		
+}
+
